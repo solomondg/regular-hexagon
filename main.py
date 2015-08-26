f920278e5de5aea7c26d5c0c15aa5352a268132a
Solomon Greenberg <solomongreenberg@gmail.com>
2015-08-26 15:18:46 -0700

    Cleaned up code, and made existing functions work. Now to start commenting.

--- main.py
+++ main.py
@@ -16,34 +16,51 @@ import random
 # import math
 from collections import namedtuple
 
-Vertex = namedtuple('vertex', 'x y')  # This'll make the vertex tuple that we'll
-                                      # use for the chromosones
+Vertex = namedtuple('vertex', 'x y')
+# This'll make the vertex tuple that we'll use for the chromosones.
+
 
 # Example chromosone
 # [vertex(1, 5), vertex(2, 6), vertex(8, 3), vertex(2, 1), vertex(8, 7),
-#   vertex(3, 2)]
+#  vertex(3, 2)]
 
 
-class Individual:
-    'Individuals for genetic algorithm. Have chromosone and related functions.'
-    def chromosonegen(self):
+# class A(object):
+#     def __init__(self):
+#         pass
+
+
+class Individual():
+    'Individuals for genetic algorithm. Has chromosone and related functions.'
+    def chromosonegen():
         """Make random chromosones, coord x, y | 0 < x < 10 """
-        V1 = Vertex(random.randrange(0, 10, 1), random.randrange(0, 10, 1))
-        V2 = Vertex(random.randrange(0, 10, 1), random.randrange(0, 10, 1))
-        V3 = Vertex(random.randrange(0, 10, 1), random.randrange(0, 10, 1))
-        V4 = Vertex(random.randrange(0, 10, 1), random.randrange(0, 10, 1))
-        V5 = Vertex(random.randrange(0, 10, 1), random.randrange(0, 10, 1))
-        V6 = Vertex(random.randrange(0, 10, 1), random.randrange(0, 10, 1))
-        return [V1, V2, V3, V4, V5, V6]
+        vert1 = Vertex(random.randrange(0, 10, 1),
+                       random.randrange(0, 10, 1))
+        vert2 = Vertex(random.randrange(0, 10, 1),
+                       random.randrange(0, 10, 1))
+        vert3 = Vertex(random.randrange(0, 10, 1),
+                       random.randrange(0, 10, 1))
+        vert4 = Vertex(random.randrange(0, 10, 1),
+                       random.randrange(0, 10, 1))
+        vert5 = Vertex(random.randrange(0, 10, 1),
+                       random.randrange(0, 10, 1))
+        vert6 = Vertex(random.randrange(0, 10, 1),
+                       random.randrange(0, 10, 1))
+        return [vert1, vert2, vert3, vert4, vert5, vert6]
 
-    def __init__(self, chromosone=chromosonegen()):
-        self.chromosone = chromosone
+    def __init__(self, input_chromosone=chromosonegen()):
+        self.chromosone = input_chromosone
         self.mutation_rate = 0.02
 
-    def point_swap(self, outside_chromosone, input_chromosone=chromosone):
+    @classmethod
+    def point_swap(self, input_chromosone, outside_chromosone):
         """Swaps genes between two points in an input and output chromosone"""
         swap_pos = random.randrange(1, 5, 1)
 #       temp_chromosone1 = chromosone[:swap_pos]
 #       temp_chromosone2 = outside_chromosone[swap_pos:]
         self.chromosone =\
-        input_chromosone[:swap_pos] + outside_chromosone[swap_pos:]
+            input_chromosone[:swap_pos] + outside_chromosone[swap_pos:]
+
+test_case = Individual()
+print test_case.chromosone
+print test_case.mutation_rate
