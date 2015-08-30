c71b5831b6ef3a4ac6addf246baf912f8705a183
Solomon Greenberg <solomongreenberg@gmail.com>
2015-08-30 10:04:20 -0700

    comments!

--- main.py
+++ main.py
@@ -55,35 +55,39 @@ class Individual():
 
 def point_swap(input_chromosone, outside_chromosone):
     """Swaps genes between two points in an input and output chromosone"""
-    swap_pos = random.randrange(1, 5, 1)
-    return input_chromosone[:swap_pos] + outside_chromosone[swap_pos:]
+    swap_pos = random.randrange(1, 5, 1)  # Randomly picks pos to swap at
+    return input_chromosone[:swap_pos] + outside_chromosone[swap_pos:]  # swaps
 
 
 def evaluator(to_eval):
     x = to_eval.chromosone
-    angle_set = [find_angle(x[5], x[0], x[1]),
-                 find_angle(x[0], x[1], x[2]),
+    angle_set = [find_angle(x[5], x[0], x[1]),  # Run the find angle function
+                 find_angle(x[0], x[1], x[2]),  # with all the vertcies
                  find_angle(x[1], x[2], x[3]),
                  find_angle(x[2], x[3], x[4]),
                  find_angle(x[3], x[4], x[5]),
                  find_angle(x[4], x[5], x[0])]
     total_error = 0
     for y in angle_set:
-        total_error += math.fabs(60-y)
+        total_error += math.fabs(60-y)  # Calculate total error with abs(60-y)
     return total_error
 
 
 def find_angle(vertA, vertB, vertC):
     # AB and BC is leg, CA is hypotenuse
+    # Find distance of segments between that vertex and neightboring ones
     ab_dist = math.sqrt((vertB.x-vertA.x)**2 + (vertB.y - vertA.y)**2)
     bc_dist = math.sqrt((vertC.x-vertB.x)**2 + (vertC.y - vertB.y)**2)
     ca_dist = math.sqrt((vertA.x-vertC.x)**2 + (vertA.y - vertC.y)**2)
+    # Calculate the angle (in radians)
     rad_angle = math.acos((ab_dist**2 + bc_dist**2 - ca_dist**2) /
                           (2*(ab_dist*bc_dist)))
-    deg_angle = rad_angle * (180/math.pi)
+    deg_angle = rad_angle * (180/math.pi)  # Change angle to degrees
 
     return deg_angle
 
+
+# Just some tests
 test_case = Individual()
 print test_case.chromosone
 print evaluator(test_case)
